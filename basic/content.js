de27e059257d1a477bbb2fb7156fe3c6a4108bbd
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-22 12:11:52 +0530

    added the extension- sites not giving permission to access aws

--- basic/content.js
+++ basic/content.js
@@ -1,8 +1,22 @@
 console.log("Extension activate");
 
-let paragraphs = document.getElementsByTagName('p');
 
-for(ele of paragraphs){
-	ele.innerHTML = "Something";
+var dotsies = new FontFace('Dotsies', 'url(https://s3.amazonaws.com/morsebeep/Dotsies.ttf)');
 
-}
+dotsies.load().then(function(loaded_face) {
+	document.fonts.add(loaded_face);
+  	document.body.style.fontFamily = '"Dotsies", Arial';
+	let paragraphs = document.getElementsByTagName('p');
+
+	for(ele of paragraphs){
+		ele.innerHTML = "Something";
+		ele.font = 'Dotsies';
+	}
+
+
+}).catch(function(error) {
+	// error occurred
+});
+
+
+
